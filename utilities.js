acb6073838efc4bb0a4432d19de074ae57644b17
nide <nenegoro@gmail.com>
2015-06-02 12:37:44 +0300

    Ported utilities to Promise

--- utilities.js
+++ utilities.js
@@ -1,3 +1,8 @@
+var Promise = require('promise-polyfill');
+var request = require('superagent');
+var url = require('url');
+
+
 module.exports = {
   REMOTE_SCHEMES: ['http', 'https', 'ftp', 'ftps'],
   NULL_VALUES: ['null', 'none', 'nil', 'nan', '-', ''],
@@ -5,26 +10,27 @@ module.exports = {
   FALSE_VALUES: ['no', 'n', 'false', 'f', '0'],
 
   // Load a JSON source, from string, URL or buffer,  into a Python type.
-  load_json_source: function (source) {
-    if(_.isNull(source) || _.isUndefined(source))
+  loadJSONSource: function (source) {
+    if(_.isNull(source) || _.isUndefined(source) || _.isString(source))
       return null;
     else if(_.isArray(source))
       // the source has already been loaded
-      return source;
+      return new Promise(function(RS, RJ) { RS(source); });
 
-    // WARN Port compat
-    if(_.contains(REMOTE_SCHEMES, compat.parse.urlparse(source).scheme))
-      // WARN Use superagent
-      source = requests.get(source).text;
+    if(_.contains(REMOTE_SCHEMES, url.parse(source).protocol.replace(':', '')))
+      return new Promise(function(RS, RJ) {
+        request
+          .get(source)
 
-    // WARN Port compat
-    else if(isinstance(source, compat.str) and not os.path.exists(source))
-      continue;
+          .end(function(E, R) {
+            if(E)
+              RJ('Failed to download registry file: ' + error);
 
-    else
-      with io.open(source, encoding='utf-8') as stream:
-        source = stream.read()
+            RS(JSON.parse(source));
+          });
 
-    return JSON.parse(source);
+      });
+
+    // WARN There is no possibility to have browser compatable code which can load file
   }
 };
