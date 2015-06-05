d36b6088ae4dfcc726b763bc85252866657a2b28
nide <nenegoro@gmail.com>
2015-06-05 23:30:14 +0300

    caseInsensitiveHeaders option test passed

--- test/models.js
+++ test/models.js
@@ -1,41 +1,46 @@
 var _ = require('underscore');
 var assert = require('chai').assert;
 var SchemaModel = require('../models');
-
-var SCHEMA = {fields: [
-  {
-    name: 'id',
-    type: 'string',
-    constraints: {required: true}
-  },
-
-  {
-    name: 'height',
-    type: 'number',
-    constraints: {required: false}
-  },
-
-  {
-    name: 'age',
-    type: 'integer',
-    constraints: {required: false}
-  },
-
-  {
-    name: 'name',
-    type: 'string',
-    constraints: {required: true}
-  },
-
-  {
-    name: 'occupation',
-    type: 'string',
-    constraints: {required: false}
-  }
-]};
+var SCHEMA;
 
 
 describe('Models', function() {
+  beforeEach(function(done, err) {
+    SCHEMA = {fields: [
+      {
+        name: 'id',
+        type: 'string',
+        constraints: {required: true}
+      },
+
+      {
+        name: 'height',
+        type: 'number',
+        constraints: {required: false}
+      },
+
+      {
+        name: 'age',
+        type: 'integer',
+        constraints: {required: false}
+      },
+
+      {
+        name: 'name',
+        type: 'string',
+        constraints: {required: true}
+      },
+
+      {
+        name: 'occupation',
+        type: 'string',
+        constraints: {required: false}
+      }
+    ]};
+
+    done();
+  });
+
   it('have a correct number of header columns', function(done, err) {
     assert.equal((new SchemaModel(SCHEMA)).headers().length, 5);
     done();
@@ -66,7 +71,20 @@ describe('Models', function() {
     done();
   });
 
-  it('respect caseInsensitiveHeaders option', function(done, err) { assert(); done(); });
+  it('respect caseInsensitiveHeaders option', function(done, err) {
+    SCHEMA.fields = _.map(SCHEMA.fields, function(F) {
+      F.name = F.name[0].toUpperCase() + _.rest(F.name).join('').toLowerCase();
+      return F;
+    });
+
+    assert.deepEqual(
+      (new SchemaModel(SCHEMA, {caseInsensitiveHeaders: true})).headers().sort(),
+      ['id', 'height', 'name', 'age', 'occupation'].sort()
+    );
+
+    done();
+  });
+
   it('raise exception when invalid json passed as schema', function(done, err) { assert(); done(); });
   it('raise exception when invalid format schema passed', function(done, err) { assert(); done(); });
 });
