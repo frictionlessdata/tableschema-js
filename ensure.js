c9e9473dc44b4cac2756a2a2ade7ecf98263e580
nide <nenegoro@gmail.com>
2015-06-05 13:05:22 +0300

    Initial porting

---
+++ ensure.js
@@ -0,0 +1,13 @@
+module.exports = function(schema) {
+	/*
+	Validate that `schema` is a valid JSON Table Schema.
+
+  Args:
+  * `schema`: a dict to check if it is valid JSON Table Schema
+
+  Returns:
+  * A tuple of `valid`, `errors`
+  */
+
+	return true;
+}
