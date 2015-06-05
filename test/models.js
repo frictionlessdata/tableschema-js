d255ce4e7d7a519a5829ab8400dff6e1148cd147
nide <nenegoro@gmail.com>
2015-06-05 22:38:41 +0300

    Empty test cases for models.js

---
+++ test/models.js
@@ -0,0 +1,13 @@
+var _ = require('underscore');
+var assert = require('chai').assert;
+
+
+describe('Models', function() {
+  it('have a correct number of header columns', function(done, err) { assert(); done(); });
+  it('have a correct number of header required columns', function(done, err) { assert(); done(); });
+  it('have one of a field from passed schema', function(done, err) { assert(); done(); });
+  it('do not have fields not specified in passed schema', function(done, err) { assert(); done(); });
+  it('respect caseInsensitiveHeaders option', function(done, err) { assert(); done(); });
+  it('raise exception when invalid json passed as schema', function(done, err) { assert(); done(); });
+  it('raise exception when invalid format schema passed', function(done, err) { assert(); done(); });
+});
