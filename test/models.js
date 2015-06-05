0c3c4700a6952a3473dafa91390634c532ca9d27
nide <nenegoro@gmail.com>
2015-06-05 23:11:14 +0300

    Required headers length test passed

--- test/models.js
+++ test/models.js
@@ -41,7 +41,11 @@ describe('Models', function() {
     done();
   });
 
-  it('have a correct number of header required columns', function(done, err) { assert(); done(); });
+  it('have a correct number of header required columns', function(done, err) {
+    assert.equal((new SchemaModel(SCHEMA)).requiredHeaders().length, 2);
+    done();
+  });
+
   it('have one of a field from passed schema', function(done, err) { assert(); done(); });
   it('do not have fields not specified in passed schema', function(done, err) { assert(); done(); });
   it('respect caseInsensitiveHeaders option', function(done, err) { assert(); done(); });
