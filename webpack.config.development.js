5a02b9075ccf6d6d1009bbb7c81581f0cee9d95f
zhenyab <zhenyab@yahoo.com>
2016-05-15 11:52:51 +0200

    Added build scripts

---
+++ webpack.config.development.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var _ = require('lodash')
+var webpack = require('webpack')
+var baseConfig = require('./webpack.config.base')
+
+var developmentConfig = {
+  output: {
+    filename: 'jsontableschema.js',
+    path: './dist'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ]
+}
+
+var config = _.merge({}, baseConfig, developmentConfig)
+
+module.exports = config
