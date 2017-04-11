ed5431f015c57a93d19f21d245c8925149d1114c
roll <eskarev@gmail.com>
2017-04-11 17:40:34 +0300

    fixed field tests (it should't use Schema)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import Table from './table'
+import Schema from './schema'
+import Field from './field'
 import validate from './validate'
 import infer from './infer'
-import Schema from './schema'
-import Table from './table'
 
-module.exports = { infer, validate, Schema, Table }
+export default {Table, Schema, Field, validate, infer}
+export {Table, Schema, Field, validate, infer}
