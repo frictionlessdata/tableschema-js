777d7926d8caa7af660190978c0f364a7ec62379
roll <eskarev@gmail.com>
2017-04-11 15:31:46 +0300

    added specs-v1 profile and up-to-date tests, updated old deps, babel, npm scripts

---
+++ test/profiles.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import {should} from 'chai'
+const profile = require('../profiles/table-schema.json')
+should()
+
+// Tests
+
+describe('profiles', () => {
+
+  it('should be up-to-date', async () => {
+    const res = await axios.get('https://specs.frictionlessdata.io/schemas/table-schema.json')
+    profile.should.deep.equal(res.data)
+  })
+
+})
