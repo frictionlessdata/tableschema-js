e9701bd3590bc350719638f4c2cf7001907c7369
roll <eskarev@gmail.com>
2017-05-03 10:22:34 +0200

    Fixed babel-polyfill

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import Table from './table'
-import Schema from './schema'
-import Field from './field'
-import validate from './validate'
-import infer from './infer'
+require('babel-polyfill')
+const Table = require('./table').Table
+const Schema = require('./schema').Schema
+const Field = require('./field').Field
+const validate = require('./validate').validate
+const infer = require('./infer').infer
 
 export default {Table, Schema, Field, validate, infer}
 export {Table, Schema, Field, validate, infer}
