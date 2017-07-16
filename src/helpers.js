5278d62774314d8bef620391e5fc1f7f3b0ba17c
roll <eskarev@gmail.com>
2017-07-16 10:17:36 +0300

    removed unused code

--- src/helpers.js
+++ src/helpers.js
@@ -58,91 +58,9 @@ function isRemotePath(path) {
 }
 
 
-// Deprecated
-
-/**
- * Check if protocol is remote.
- */
-function isURL(protocol) {
-  const REMOTE_SCHEMES = ['http', 'https', 'ftp', 'ftps']
-  if (!protocol) return false
-  return REMOTE_SCHEMES.indexOf(protocol.replace(':', '')) !== -1
-}
-
-
-/**
- * Check unique constraints for every header and value independently.
- * Does not take in count the case, when headers which construct primary key
- * should be checked in combination with each other
- *
- * @param fieldName
- * @param value
- * @param headers
- * @param unique
- */
-function checkUnique(fieldName, value, headers, unique) {
-  const _ = lodash
-  if (!_.includes(headers, fieldName)) {
-    return
-  }
-
-  if (!Object.prototype.hasOwnProperty.call(unique, fieldName)) {
-    unique[fieldName] = [value]
-  } else {
-    if (_.includes(unique[fieldName], value)) {
-      throw new UniqueConstraintsError(
-          `Unique constraint violation for field name '${fieldName}'`)
-    }
-    unique[fieldName].push(value)
-  }
-}
-
-
-/**
- * Check uniqueness of primary key
- *
- * @param values
- * @param headers
- * @param unique
- */
-function checkUniquePrimary(values, headers, unique) {
-  const _ = lodash
-  const key = _.keys(headers).join('')
-    , indexes = _.values(headers)
-
-  let value = ''
-
-  if (!Object.prototype.hasOwnProperty.call(unique, key)) {
-    unique[key] = []
-  }
-
-  _.forEach(indexes, index => {
-    value += values[index].toString()
-  })
-
-  if (_.includes(unique[key], value)) {
-    throw new UniqueConstraintsError('Unique constraint violation for primary key')
-  }
-  unique[key].push(value)
-}
-
-
-class UniqueConstraintsError extends Error {
-  constructor(message) {
-    super(message)
-    this.message = message
-    this.name = 'UniqueConstraintsError'
-  }
-}
-
-
 module.exports = {
   retrieveDescriptor,
   expandSchemaDescriptor,
   expandFieldDescriptor,
   isRemotePath,
-  isURL,
-  checkUnique,
-  checkUniquePrimary,
-  UniqueConstraintsError,
 }
