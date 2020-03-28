711e5abf780e2d96de15ac4063f4397643a32620
roll <eskarev@gmail.com>
2020-03-28 16:34:35 +0300

    Added a failing test

--- src/table.js
+++ src/table.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const csv = require('csv')
 const axios = require('axios')
-const {Readable, PassThrough} = require('stream')
+const { Readable, PassThrough } = require('stream')
 const zip = require('lodash/zip')
 const isEqual = require('lodash/isEqual')
 const isArray = require('lodash/isArray')
@@ -11,11 +11,10 @@ const isFunction = require('lodash/isFunction')
 const zipObject = require('lodash/zipObject')
 const S2A = require('stream-to-async-iterator').default
 const CSVSniffer = require('csv-sniffer')()
-const {TableSchemaError} = require('./errors')
-const {Schema} = require('./schema')
+const { TableSchemaError } = require('./errors')
+const { Schema } = require('./schema')
 const helpers = require('./helpers')
 const config = require('./config')
-
 
 // Module API
 
@@ -23,7 +22,6 @@ const config = require('./config')
  * Table representation
  */
 class Table {
-
   // Public
 
   /**
@@ -52,21 +50,23 @@ class Table {
    * @returns {Table} data table class instance
    *
    */
-  static async load(source, {
-    schema,
-    strict=false,
-    headers=1,
-    format=config.DEFAULT_FORMAT,
-    encoding=config.DEFAULT_ENCODING,
-    ...parserOptions
-  }={}) {
-
+  static async load(
+    source,
+    {
+      schema,
+      strict = false,
+      headers = 1,
+      format = config.DEFAULT_FORMAT,
+      encoding = config.DEFAULT_ENCODING,
+      ...parserOptions
+    } = {}
+  ) {
     // Load schema
     if (schema && !(schema instanceof Schema)) {
-      schema = await Schema.load(schema, {strict})
+      schema = await Schema.load(schema, { strict })
     }
 
-    return new Table(source, {schema, strict, headers, format, encoding, ...parserOptions})
+    return new Table(source, { schema, strict, headers, format, encoding, ...parserOptions })
   }
 
   /**
@@ -112,7 +112,14 @@ class Table {
    *  - `{header1: value1, header2: value2}` - keyed
    *  - `[rowNumber, [header1, header2], [value1, value2]]` - extended
    */
-  async iter({keyed, extended, cast=true, relations=false, stream=false, forceCast=false}={}) {
+  async iter({
+    keyed,
+    extended,
+    cast = true,
+    relations = false,
+    stream = false,
+    forceCast = false,
+  } = {}) {
     const source = this._source
 
     // Prepare unique checks
@@ -128,88 +135,95 @@ class Table {
 
     // Get table row stream
     let rowNumber = 0
-    const tableRowStream = rowStream.pipe(csv.transform(row => {
-      rowNumber += 1
+    const tableRowStream = rowStream.pipe(
+      csv.transform(row => {
+        rowNumber += 1
 
-      // Get headers
-      if (rowNumber === this._headersRow) {
-        this._headers = row
-        return
-      }
-
-      // Check headers
-      if (cast) {
-        if (this.schema && this.headers) {
-          if (!isEqual(this.headers, this.schema.fieldNames)) {
-            const error = new TableSchemaError(
-              'The column header names do not match the field names in the schema')
-            error.rowNumber = rowNumber
-            error.headerNames = this.headers
-            error.fieldNames = this.fieldNames
-            if (forceCast) return error
-            throw error
-          }
+        // Get headers
+        if (rowNumber === this._headersRow) {
+          this._headers = row
+          return
         }
-      }
 
-      // Cast row
-      if (cast) {
-        if (this.schema) {
-          try {
-            row = this.schema.castRow(row, {failFast: false})
-          } catch (error) {
-            error.rowNumber = rowNumber
-            error.errors.forEach(error => {error.rowNumber = rowNumber})
-            if (forceCast) return error
-            throw error
-          }
-        }
-      }
-
-      // Check unique
-      if (cast) {
-        for (const [indexes, cache] of Object.entries(uniqueFieldsCache)) {
-          const splitIndexes = indexes.split(',').map(index => parseInt(index, 10))
-          const values = row.filter((value, index) => splitIndexes.includes(index))
-          if (!values.every(value => value === null)) {
-            if (cache.data.has(values.toString())) {
+        // Check headers
+        if (cast) {
+          if (this.schema && this.headers) {
+            if (!isEqual(this.headers, this.schema.fieldNames)) {
               const error = new TableSchemaError(
-                `Row ${rowNumber} has an unique constraint ` +
-                `violation in column "${cache.name}"`)
+                'The column header names do not match the field names in the schema'
+              )
               error.rowNumber = rowNumber
-              if (forceCast) return error
-              throw error
-            }
-            cache.data.add(values.toString())
-          }
-        }
-      }
-
-      // Resolve relations
-      if (relations) {
-        if (this.schema) {
-          for (const foreignKey of this.schema.foreignKeys) {
-            row = resolveRelations(row, this.headers, relations, foreignKey)
-            if (row === null) {
-              const error = new TableSchemaError(
-                `Foreign key "${foreignKey.fields}" violation in row ${rowNumber}`)
-              error.rowNumber = rowNumber
+              error.headerNames = this.headers
+              error.fieldNames = this.fieldNames
               if (forceCast) return error
               throw error
             }
           }
         }
-      }
 
-      // Form row
-      if (keyed) {
-        row = zipObject(this.headers, row)
-      } else if (extended) {
-        row = [rowNumber, this.headers, row]
-      }
+        // Cast row
+        if (cast) {
+          if (this.schema) {
+            try {
+              row = this.schema.castRow(row, { failFast: false })
+            } catch (error) {
+              error.rowNumber = rowNumber
+              error.errors.forEach(error => {
+                error.rowNumber = rowNumber
+              })
+              if (forceCast) return error
+              throw error
+            }
+          }
+        }
 
-      return row
-    }))
+        // Check unique
+        if (cast) {
+          for (const [indexes, cache] of Object.entries(uniqueFieldsCache)) {
+            const splitIndexes = indexes.split(',').map(index => parseInt(index, 10))
+            const values = row.filter((value, index) => splitIndexes.includes(index))
+            if (!values.every(value => value === null)) {
+              if (cache.data.has(values.toString())) {
+                const error = new TableSchemaError(
+                  `Row ${rowNumber} has an unique constraint ` +
+                    `violation in column "${cache.name}"`
+                )
+                error.rowNumber = rowNumber
+                if (forceCast) return error
+                throw error
+              }
+              cache.data.add(values.toString())
+            }
+          }
+        }
+
+        // Resolve relations
+        if (relations) {
+          if (this.schema) {
+            for (const foreignKey of this.schema.foreignKeys) {
+              row = resolveRelations(row, this.headers, relations, foreignKey)
+              if (row === null) {
+                const error = new TableSchemaError(
+                  `Foreign key "${foreignKey.fields}" violation in row ${rowNumber}`
+                )
+                error.rowNumber = rowNumber
+                if (forceCast) return error
+                throw error
+              }
+            }
+          }
+        }
+
+        // Form row
+        if (keyed) {
+          row = zipObject(this.headers, row)
+        } else if (extended) {
+          row = [rowNumber, this.headers, row]
+        }
+
+        return row
+      })
+    )
 
     // Handle csv errors
     rowStream.on('error', () => {
@@ -223,8 +237,8 @@ class Table {
     }
 
     // Return iterator
+    // return Symbol.asyncIterator in tableRowStream ? tableRowStream : new S2A(tableRowStream)
     return new S2A(tableRowStream)
-
   }
 
   /**
@@ -238,10 +252,9 @@ class Table {
    *  - `{header1: value1, header2: value2}` - keyed
    *  - `[rowNumber, [header1, header2], [value1, value2]]` - extended
    */
-  async read({keyed, extended, cast=true, relations=false, limit, forceCast=false}={}) {
-
+  async read({ keyed, extended, cast = true, relations = false, limit, forceCast = false } = {}) {
     // Get rows
-    const iterator = await this.iter({keyed, extended, cast, relations, forceCast})
+    const iterator = await this.iter({ keyed, extended, cast, relations, forceCast })
     const rows = []
     let count = 0
     for (;;) {
@@ -249,7 +262,7 @@ class Table {
       const iteration = await iterator.next()
       if (iteration.done) break
       rows.push(iteration.value)
-      if (limit && (count >= limit)) break
+      if (limit && count >= limit) break
     }
 
     return rows
@@ -263,19 +276,17 @@ class Table {
    * @param {number} limit - limit rows sample size
    * @returns {Object} Table Schema descriptor
    */
-  async infer({limit=100}={}) {
+  async infer({ limit = 100 } = {}) {
     if (!this._schema || !this._headers) {
-
       // Headers
-      const sample = await this.read({limit, cast: false})
+      const sample = await this.read({ limit, cast: false })
 
       // Schema
       if (!this.schema) {
         const schema = new Schema()
-        schema.infer(sample, {headers: this.headers})
-        this._schema = new Schema(schema.descriptor, {strict: this._strict})
+        schema.infer(sample, { headers: this.headers })
+        this._schema = new Schema(schema.descriptor, { strict: this._strict })
       }
-
     }
     return this._schema.descriptor
   }
@@ -288,22 +299,24 @@ class Table {
    * @returns {Boolean} true on success
    */
   async save(target) {
-    const rowStream = await this.iter({keyed: true, stream: true})
-    const textStream = rowStream.pipe(csv.stringify({header: true}))
+    const rowStream = await this.iter({ keyed: true, stream: true })
+    const textStream = rowStream.pipe(csv.stringify({ header: true }))
     textStream.pipe(fs.createWriteStream(target))
   }
 
   // Private
 
-  constructor(source, {
-    schema,
-    strict=false,
-    headers=1,
-    format=config.DEFAULT_FORMAT,
-    encoding=config.DEFAULT_ENCODING,
-    ...parserOptions
-  }={}) {
-
+  constructor(
+    source,
+    {
+      schema,
+      strict = false,
+      headers = 1,
+      format = config.DEFAULT_FORMAT,
+      encoding = config.DEFAULT_ENCODING,
+      ...parserOptions
+    } = {}
+  ) {
     // Not supported formats
     if (!['csv'].includes(format)) {
       throw new TableSchemaError(`Tabular format "${format}" is not supported`)
@@ -326,33 +339,31 @@ class Table {
       this._headersRow = headers
     }
   }
-
 }
-
 
 // Internal
 
 async function createRowStream(source, encoding, parserOptions) {
-  const parser = csv.parse({ltrim: true, relax_column_count: true, ...parserOptions})
+  const parser = csv.parse({ ltrim: true, relax_column_count: true, ...parserOptions })
   let stream
 
   // Stream factory
   if (isFunction(source)) {
     stream = source()
 
-  // Node stream
+    // Node stream
   } else if (source.readable) {
     stream = source
 
-  // Inline source
+    // Inline source
   } else if (isArray(source)) {
-    stream = new Readable({objectMode: true})
+    stream = new Readable({ objectMode: true })
     for (const row of source) stream.push(row)
     stream.push(null)
 
-  // Remote source
-  // For now only utf-8 encoding is supported:
-  // https://github.com/axios/axios/issues/332
+    // Remote source
+    // For now only utf-8 encoding is supported:
+    // https://github.com/axios/axios/issues/332
   } else if (helpers.isRemotePath(source)) {
     if (config.IS_BROWSER) {
       const response = await axios.get(source)
@@ -360,11 +371,11 @@ async function createRowStream(source, encoding, parserOptions) {
       stream.push(response.data)
       stream.push(null)
     } else {
-      const response = await axios.get(source, {responseType: 'stream'})
+      const response = await axios.get(source, { responseType: 'stream' })
       stream = response.data
     }
 
-  // Local source
+    // Local source
   } else {
     if (config.IS_BROWSER) {
       throw new TableSchemaError('Local paths are not supported in the browser')
@@ -402,7 +413,7 @@ function createCsvDelimiterDetector(csvParser) {
   const sniffer = new CSVSniffer()
   let done = false
 
-  detector.on('data', (chunk) => {
+  detector.on('data', chunk => {
     if (!done) {
       const result = sniffer.sniff(chunk.toString())
       csvParser.options.delimiter = result.delimiter
@@ -412,7 +423,6 @@ function createCsvDelimiterDetector(csvParser) {
 
   return detector
 }
-
 
 function createUniqueFieldsCache(schema) {
   const primaryKeyIndexes = []
@@ -443,9 +453,7 @@ function createUniqueFieldsCache(schema) {
   return cache
 }
 
-
 function resolveRelations(row, headers, relations, foreignKey) {
-
   // Prepare helpers - needed data structures
   const keyedRow = new Map(zip(headers, row))
   const fields = zip(foreignKey.fields, foreignKey.reference.fields)
@@ -478,7 +486,6 @@ function resolveRelations(row, headers, relations, foreignKey) {
   }
 
   return valid ? Array.from(keyedRow.values()) : null
-
 }
 
 // System
