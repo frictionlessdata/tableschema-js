abaab734c5ff2eb77f05be7bfd123673b7890c20
roll <eskarev@gmail.com>
2020-03-30 14:13:51 +0300

    Rebase on moment for time

--- src/types/time.js
+++ src/types/time.js
@@ -1,8 +1,8 @@
 const moment = require('moment')
 const isDate = require('lodash/isDate')
 const isString = require('lodash/isString')
-const { timeParse } = require('d3-time-format')
 const { ERROR } = require('../config')
+const helpers = require('../helpers')
 
 // Module API
 
@@ -13,7 +13,7 @@ function castTime(format, value) {
     }
     try {
       if (format === 'default') {
-        value = moment(timeParse(_DEFAULT_PATTERN)(value))
+        value = moment(value, _DEFAULT_PATTERN, true)
       } else if (format === 'any') {
         try {
           moment.suppressDeprecationWarnings = true
@@ -29,7 +29,7 @@ function castTime(format, value) {
           )
           format = format.replace('fmt:', '')
         }
-        value = moment(timeParse(format)(value))
+        value = moment(value, helpers.convertDatetimeFormatFromFDtoJS(format), true)
       }
       if (!value.isValid()) {
         return ERROR
@@ -48,4 +48,4 @@ module.exports = {
 
 // Internal
 
-const _DEFAULT_PATTERN = '%H:%M:%S'
+const _DEFAULT_PATTERN = 'HH:mm:ss'
