58464f82831dc7588ab44f2a165009e01dc00767
roll <eskarev@gmail.com>
2020-04-30 10:44:15 +0300

    Added support for infinite number

--- src/types/number.js
+++ src/types/number.js
@@ -7,11 +7,14 @@ const { ERROR } = require('../config')
 // Module API
 
 function castNumber(format, value, options = {}) {
-  const decimalChar = options.decimalChar || _DEFAULT_DECIMAL_CHAR
-  const groupChar = options.groupChar || _DEFAULT_GROUP_CHAR
   if (!isNumber(value)) {
     if (!isString(value)) return ERROR
     if (!value.length) return ERROR
+    if (value === 'NaN') return NaN
+    if (value === 'INF') return Infinity
+    if (value === '-INF') return -Infinity
+    const decimalChar = options.decimalChar || _DEFAULT_DECIMAL_CHAR
+    const groupChar = options.groupChar || _DEFAULT_GROUP_CHAR
     value = value.replace(new RegExp('\\s', 'g'), '')
     value = value.replace(new RegExp(`[${decimalChar}]`, 'g'), '.')
     value = value.replace(new RegExp(`[${groupChar}]`, 'g'), '')
@@ -23,9 +26,9 @@ function castNumber(format, value, options = {}) {
     } catch (error) {
       return ERROR
     }
-  }
-  if (isNaN(value)) {
-    return ERROR
+    if (isNaN(value)) {
+      return ERROR
+    }
   }
   return value
 }
